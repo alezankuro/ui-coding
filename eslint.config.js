6649eabb85a0a21ffa2de6e0ca36f7a8d0f9a05e
Alexanedr Zankevich <Aliaksandr.Zankevich@gmail.com>
2025-04-06 18:23:08 +0300

    chore(): Add ESLint plugin to sort imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,17 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 export default tseslint.config(
     { ignores: ['dist'] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
-        files: ['**/*.{ts,tsx}'],
+        files: ['**/*.{ts,tsx,js,jsx}'],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
@@ -18,11 +19,34 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'simple-import-sort': simpleImportSort,
             prettier: prettierPlugin,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'simple-import-sort/imports': [
+                'error',
+                {
+                    groups: [
+                        // Node.js builtins
+                        ['^node:'],
+                        // Packages. `react` related packages come first.
+                        ['^react', '^\\w', '^@?\\w'],
+                        // Internal packages.
+                        ['^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)'],
+                        // Side effect imports.
+                        ['^\\u0000'],
+                        // Parent imports. Put `..` last.
+                        ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                        // Other relative imports. Put same-folder imports and `.` last.
+                        ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                        // Style imports.
+                        ['^.+\\.s?css$'],
+                    ],
+                },
+            ],
+            'simple-import-sort/exports': 'error',
             'prettier/prettier': 'error',
         },
     },
