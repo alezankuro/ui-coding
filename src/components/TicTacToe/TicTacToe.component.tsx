0a36b31809f27646824b53b7d31de51cd585d9b2
Alexanedr Zankevich <Aliaksandr.Zankevich@gmail.com>
2025-05-22 04:03:00 +0300

    feat(tic-tac-toe): Add view trainsition

--- src/components/TicTacToe/TicTacToe.component.tsx
+++ src/components/TicTacToe/TicTacToe.component.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useEffect, useState } from 'react';
+import { CSSProperties, useCallback, useEffect, useRef, useState } from 'react';
 
 import Board from './Board';
 import { BoardCellValue, CellValueMap, DEFAULT_BOARD_SIZE } from './TicTacToe.constants';
@@ -77,10 +77,73 @@ interface ControlsProps {
 }
 
 export function Controls({ winner, onReset }: ControlsProps) {
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+    const openModal = useCallback(() => {
+        const dialog = dialogRef.current;
+        const button = buttonRef.current;
+
+        if (dialog?.open) return;
+
+        document.startViewTransition(() => {
+            if (button) button.style.display = 'none';
+            if (dialog) dialog.showModal();
+        });
+    }, []);
+
+    const closeModal = useCallback(() => {
+        const dialog = dialogRef.current;
+        const button = buttonRef.current;
+
+        document.startViewTransition(() => {
+            if (dialog) dialog.close();
+            if (button) button.style.display = 'block';
+        });
+    }, []);
+
+    useEffect(() => {
+        if (winner) openModal();
+    }, [winner, openModal]);
+
+    useEffect(() => {
+        const dialog = dialogRef.current;
+
+        function onClose(event: Event) {
+            event.preventDefault();
+            closeModal();
+        }
+
+        if (dialog) dialog.addEventListener('cancel', onClose);
+
+        return () => {
+            if (dialog) dialog.removeEventListener('cancel', onClose);
+        };
+    }, [closeModal]);
+
+    function onPlayAgain() {
+        onReset();
+        closeModal();
+    }
+
     return (
         <div className="tic-tac-controls">
-            {winner ? <div>Winner: {winner}</div> : null}
-            <button onClick={onReset}>Restart</button>
+            <button
+                ref={buttonRef}
+                style={{ viewTransitionName: 'reset-button' }}
+                onClick={onReset}
+            >
+                Restart
+            </button>
+            <dialog ref={dialogRef} className="tic-tac-winner-dialog">
+                <div className="winner-dialog-container">
+                    <div className="winner-dialog-header">Winner</div>
+                    <div className="winner-dialog-content">{winner && CellValueMap[winner]}</div>
+                    <button style={{ viewTransitionName: 'reset-button' }} onClick={onPlayAgain}>
+                        Play again
+                    </button>
+                </div>
+            </dialog>
         </div>
     );
 }
